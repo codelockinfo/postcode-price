70e0194990928db5c78f44418154217847469bc2
cls007 <dhara.codelock99@gamil.com>
2023-09-23 12:14:36 +0530

    help page design set and link a page on index page

--- user/help.php
+++ user/help.php
@@ -1,111 +1,65 @@
 <?php include_once('cls_header.php'); 
     include_once('dashboard_header.php');
 ?>
-<div class="Polaris-Page pagemargin max_width_change bodycontainer">
+<div class="Polaris-Page pagemargin max_width_change bodycontainer cus_help">
     
     <div class="Polaris-Page__Header Polaris-Page__Header--hasBreadcrumbs Polaris-Page__Header--hasSecondaryActions Polaris-Page__Header--hasSeparator">
         <div class="Polaris-Page__MainContent">
             <div class="Polaris-Page__TitleAndActions">
                 <div class="Polaris-Page__Title">
-                    <h1 class="Polaris-DisplayText Polaris-DisplayText--sizeLarge"><?php _e("Support/FAQ"); ?></h1>
+                    <h1 class="Polaris-DisplayText Polaris-DisplayText--sizeLarge">How To Configure?</h1>
+                    <p>Follow the steps below to add a ZIP Code widget to your store.</p>
                 </div>
             </div>
         </div>
     </div>
-    <div class="box text-center space-y-2 py-6">
-        <h2 class="text-2xl">Still need help?</h2>
-        <p class="text-lg text-gray-500">Email us at <strong class="font-semibold">codelock2021@gmail.com</strong>. Remember to include your myshopify.com domain.</p>
-    </div>
-    <div class="Polaris-Page__Content">
-        <div class="Polaris-Layout">
-            <div class="Polaris-Layout__Section">
-                <div class="Polaris-Card">
-                    <!-- <div class="Polaris-Card__Header">
-                        <h2 class="Polaris-Heading">FAQ</h2>
-                    </div> -->
-                    <div class="Polaris-Card__Section">
-                        <div  class="acc">
-                            <div class="acc__card">
-                                <div class="acc__title active">
-                                    <a class="card-title">
-                                    How to design the app to match your store
-                                    </a>
-                                </div>
-                                <div  class="acc__panel" style="display:block;" >
-                                    <div class="card-body">
-                                       <div class="videocontent">
-                                            <ul>
-                                                <li><strong>Option A:</strong> </li><br>
-                                                <li>Using our quick themes.</li>
-                                                <li>We offer pre-made design themes you can use on your store.</li>
-                                                <li> Click any of the following to apply a quick design to your app:</li>
-                                                <p><img src="<?php echo CLS_SITE_URL; ?>/assets/images/help/1.png"  alt="doc image"></p>
-                                                <li><strong>Option B:</strong></li> <br>
-                                                <li>Manually changing the app design.</li>
-                                                <li>Scroll down the settings panel to change colors and fonts.</li>
-                                                <li>We recommend starting from selecting a "Banner" or a "pop-up" layout.</li>
-                                                <p><img src="<?php echo CLS_SITE_URL; ?>/assets/images/help/2.png"  alt="doc image"></p>
-                                                <li>That's it!</li>
-                                                <li>Feel free to reach out and ask for design help - our team is at your service: codelock2021@gmail.com</li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="acc__card">
-                                <div class="acc__title">
-                                    <a class="card-title">
-                                    How to change and edit the text  of your banner?
-                                    </a>
-                                </div>
-                                <div  class="acc__panel" >
-                                    <div class="card-body">
-                                        <div class="videocontent">
-                                            <ul>
-                                                <li><img src="<?php echo CLS_SITE_URL; ?>/assets/images/help/3.png"  alt="doc image"></li>
-                                                <li>You can change the text banner on this label message.</li>
-                                                <li>Then modified the agreement text and link text.</li>
-                                                <li>You can also change the font size of the banner and also change the height of the banner.</li>
-                                                <li>Then click on the <b>Save</b> button.</li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="acc__card">
-                                <div class="acc__title">
-                                    <a class="card-title">
-                                    How I can revert my font back to the original?
-                                    </a>
-                                </div>
-                                <div class="acc__panel" >
-                                    <div class="card-body">
-                                            <div class="col-sm-12">
-                                            Very simple, you just need to deactivate the Easy Cookie banner App or remove the fonts. it will revert to original font of the theme.
-                                            </div>
-                                    </div>
-                                </div>
-                            </div>
-                           
-                            
-                        </div>
-                    </div>
-                </div>
+   
+    <div class="featurecontent">
+            <div class="fea_con" style="margin-top: 20px;">
+            <div class="col-md-1">
+                <img src="<?php echo CLS_SITE_URL; ?>/assets/images/post/1.png" width="50px" alt="">
+            </div>
+            <div class="col-md-11">
+                <b>Set List:</b> Navigate to the
+                <a href="addzone.php?store=<?php echo $_SESSION['store'];?>" target="_blank">ZIP Code Sets
+                </a> page. There will already be a default set. Make sure its status is set to On.
+            </div>
+            </div>
+            <div class="fea_con" style="margin-top: 20px;">
+            <div class="col-md-1">
+                <img src="<?php echo CLS_SITE_URL; ?>/assets/images/post/2.png" width="50px" alt="">
+            </div>
+            <div class="col-md-11">
+                <b>Edit The Default Set:</b> Add/import ZIP Codes and make changes according to your need.
+            </div>
+            </div>
+            <div class="fea_con" style="margin-top: 20px;">
+            <div class="col-md-1">
+                <img src="<?php echo CLS_SITE_URL; ?>/assets/images/post/3.png" width="50px" alt="">
+            </div>
+            <div class="col-md-11">
+                <b>Design Zippy Widget:</b> Navigate to the
+                <a href="setting.php?store=<?php echo $_SESSION['store'];?>" target="_blank">Appearance</a> page and customize the Zippy widget as you want.
+            </div>
+            </div>
+            <div class="fea_con" style="margin-top: 20px; margin-bottom: 25px;">
+            <div class="col-md-1">
+                <img src="<?php echo CLS_SITE_URL; ?>/assets/images/post/4.png" width="50px" alt="">
+            </div>
+            <div class="col-md-11">
+                <b>Position The Zippy Widget:</b> Go to the
+                <a href="setting.php?store=<?php echo $_SESSION['store'];?>" target="_blank">Settings</a> page, choose a widget position, and save. You can also set the display rules here.
+            </div>
+            </div>
+            <div class="fea_con" style="margin-top: 20px; margin-bottom: 25px;">
+            <div class="col-md-1">
+                <img src="<?php echo CLS_SITE_URL; ?>/assets/images/post/5.png" width="50px" alt="">
+            </div>
+            <div class="col-md-11">
+                <b>Test It:</b> Make a proper testing before making it live to your store. Feel free to contact us if need any help or have any questions.
+            </div>
             </div>
         </div>
-    </div>
-    <div class="Polaris-Banner Polaris-Banner--statusInfo Polaris-Banner--withinPage bannermargin" tabindex="0" role="status" aria-live="polite" aria-labelledby="Banner18Heading" aria-describedby="Banner18Content">
-        <div class="Polaris-Banner__Ribbon"><span class="Polaris-Icon Polaris-Icon--colorTealDark Polaris-Icon--hasBackdrop"><svg class="Polaris-Icon__Svg" viewBox="0 0 20 20"><g fill-rule="evenodd"><circle cx="10" cy="10" r="9" fill="currentColor"></circle><path d="M10 0C4.486 0 0 4.486 0 10s4.486 10 10 10 10-4.486 10-10S15.514 0 10 0m0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8m1-5v-3a1 1 0 0 0-1-1H9a1 1 0 1 0 0 2v3a1 1 0 0 0 1 1h1a1 1 0 1 0 0-2m-1-5.9a1.1 1.1 0 1 0 0-2.2 1.1 1.1 0 0 0 0 2.2"></path></g></svg></span></div>
-            <div>
-                <div class="Polaris-Banner__Heading" id="Banner18Heading">
-                    <p class="Polaris-Heading"><?php _e("Need any other help?"); ?></p>
-                </div>
-                <div class="Polaris-Banner__Content" id="Banner6Content">
-                    <p>We are always here to help you. Please contact us on our email codelockinfo@gmail.com</p>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
     
 <?php include_once('dashboard_footer.php'); ?>
