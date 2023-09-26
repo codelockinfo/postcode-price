cd97e6f18520da52f3b08a9f965e9b30e6d9e3f3
codelockinfo <codelockinfo@gmail.com>
2023-09-26 16:48:32 +0530

    result of select function print in get postcode function

--- user/cls_functions.php
+++ user/cls_functions.php
@@ -524,11 +524,9 @@ class Client_functions extends common_function {
             $zonearea = isset($_POST['postcode']) ? $_POST['postcode'] : '';
             $where_query = array(["", "zonearea", "=", "$zonearea"],["AND", "store_user_id", "=", $shopinfo->store_user_id]);
             $comeback = $this->select_result(TABLE_ZONE_MASTER, '*', $where_query);
-            echo "<pre>";
-            print_r($comeback);
             generate_log("Comeback",json_encode($comeback));
-            if (!empty($comeback["data"])) {
-                $data = (object)$comeback["data"];
+            if (!empty($comeback["data"][0])) {
+                $data = (object)$comeback["data"][0];
                 $return_arary["zoneprice"] = isset($data->zoneprice) ? $data->zoneprice : '';
                 $return_arary["zonename"] = isset($data->zonename) ? $data->zonename : '';
                 $return_arary["zonearea"] = isset($data->zonearea) ? $data->zonearea : '';
