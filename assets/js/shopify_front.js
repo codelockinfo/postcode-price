f68257cb8e8e71783bca8245ea6f988ddfe1e085
codelockinfo <codelockinfo@gmail.com>
2023-10-02 13:11:43 +0530

    redirect changes url

--- assets/js/shopify_front.js
+++ assets/js/shopify_front.js
@@ -403,7 +403,7 @@ include('https://code.jquery.com/jquery-3.6.0.min.js', function() {
             console.log("clsremovezipcode");
             console.log(shop);
             deleteCookie("postcodeval");
-            url = shop;
+            url = 'https://'+shop;
             window.location.replace(url);
             $("#postalholder").css("opacity","0");
         });
