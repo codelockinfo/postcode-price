3def0f3fe94c7eb38d63e63c5624b1ef01ee9874
codelockinfo <codelockinfo@gmail.com>
2023-10-17 09:16:35 +0530

    submit zipcode add otpion html

--- assets/js/shopify_front.js
+++ assets/js/shopify_front.js
@@ -182,7 +182,15 @@ include('https://code.jquery.com/jquery-3.6.0.min.js', function() {
                         $productPriceClass = $(".price .price__regular .price-item");
                             if (comeback['code'] != undefined && comeback['code'] == '403') {
                             }else if (comeback['outcome'] == 'true') {
-                                    $("#clsproductZipcodevalue").val(getpostcode);
+                                
+                                    var inputZipcodeValue = $('input[name="clsproductZipcodevalue"]');
+                                    var inputZipcodeValue = inputZipcodeValue.val();
+                                    if($inputZipcodeValue == undefined){
+                                        $(".product-form").append('<input type="hidden" name="clsproductxipcodevalue" id="clsproductZipcodevalue" value="">'+
+                                        '<input type="hidden" name="clsoption0" id="clsoption0" value="">'+
+                                        '<input type="hidden" name="clsoption1" id="clsoption1" value="">');
+                                    }
+                                    inputZipcodeValue.val(getpostcode);
                                     $ProductPriceSymbol =  $.trim($productPriceClass.html());
                                     $(".chkpostcode").html("");
                                     $zonename = (comeback.data['zonename']);
