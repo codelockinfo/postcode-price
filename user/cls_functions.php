f0a04d6854b40c0c9acc17d1731f1fcdf8da4867
codelockinfo <codelockinfo@gmail.com>
2023-10-23 18:00:49 +0530

    variant title remove

--- user/cls_functions.php
+++ user/cls_functions.php
@@ -702,7 +702,6 @@ class Client_functions extends common_function {
 
         if ($productdata && isset($productdata->product->variants)) {
             $variants_count = count($productdata->product->variants);
-            generate_log('createproduct', $productdata->product->variants['title']);
             generate_log('createproduct', $variants_count . "  variant count"); 
         } else {
             generate_log('createproduct',"  variant not found"); 
