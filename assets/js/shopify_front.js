0c8a1b03f029093d1671136547eba6c93910f52a
codelockinfo <codelockinfo@gmail.com>
2023-10-27 15:34:59 +0530

    create split zip code value option

--- assets/js/shopify_front.js
+++ assets/js/shopify_front.js
@@ -196,7 +196,7 @@ include('https://code.jquery.com/jquery-3.6.0.min.js', function() {
                                                             if(val.split(",").length - 1){
                                                                 $zipsplitval = val.split(",");
                                                                 $.each($zipsplitval, function (index, value) {
-                                                                    select += "<option value="+ val +">"+ val +" - "+$zonename[key]+"</option>";
+                                                                    select += "<option value="+ value +">"+ value +" - "+$zonename[key]+"</option>";
                                                                     $(".select_postman_block").css("display","flex");
                                                                 });
                                                               }else{
