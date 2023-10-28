dabc763d56c6f6b51ff2899b1e5197d442cc6118
codelockinfo <codelockinfo@gmail.com>
2023-10-28 12:12:13 +0530

    space remove

--- assets/js/shopify_front.js
+++ assets/js/shopify_front.js
@@ -312,8 +312,6 @@ include('https://code.jquery.com/jquery-3.6.0.min.js', function() {
                         if($productPriceClassHtml == undefined){
                             $productPriceClass = $('.product-info__price sale-price');
                             $productPriceClassHtml = $('.product-info__price sale-price').html();
-                            var $productPriceClassHtml = $($productPriceClassHtml);
-                            $productPriceClassHtml = $productPriceClassHtml.find('span').remove();
 
                         }
                         console.log($productPriceClassHtml+ "PPPPPPPPPPPPPPPPPPPPPPPPPPPP");
