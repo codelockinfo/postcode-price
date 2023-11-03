522032a2fbf57a9c70f382a3769dd32466fe16e1
codelockinfo <codelockinfo@gmail.com>
2023-11-03 15:36:04 +0530

    onload variant set in broadcast theme

--- assets/js/shopify_front.js
+++ assets/js/shopify_front.js
@@ -434,7 +434,18 @@ include('https://code.jquery.com/jquery-3.6.0.min.js', function() {
                                     $dropdownval = ($(this).find(".select__select").val() == '') ? '' : $(this).find(".select__select").val();
                                     (index == 0) ? $("#clsoption0").val($dropdownval) :  $("#clsoption1").val($dropdownval);
                                 });
-
+                                if($("#clsoption0").val() == "" ){
+                                    console.log("option0");
+                                   $clsoption0 =  $('input[data-index="option1"]').val();
+                                   console.log($clsoption0);
+                                   $("#clsoption0").val($clsoption0);
+                                }
+                                if($("#clsoption1").val() == "" ){
+                                    console.log("option1");
+                                    $clsoption1 =  $('input[data-index="option2"]').val();
+                                    console.log($clsoption1);
+                                    $("#clsoption1").val($clsoption1);
+                                 }
 
                         } else {
                             $(".clssucessmsg").html("");
