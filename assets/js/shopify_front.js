404acf42882a9568925a06321a6497b284e774ae
codelockinfo <codelockinfo@gmail.com>
2023-11-04 10:44:44 +0530

    reert avove add

--- assets/js/shopify_front.js
+++ assets/js/shopify_front.js
@@ -547,7 +547,7 @@ include('https://code.jquery.com/jquery-3.6.0.min.js', function() {
             $paytment_Btn_text = $(".shopify-payment-button__button").html();
             $(".shopify-payment-button__button").css("display","none");
             $payment_btn_Html = '<button class="'+ $paytment_Btn_class +' clspayment">'+ $paytment_Btn_text +'</button>';
-            $(".product-form__buttons,.ProductForm,.product__submit__buttons").append($payment_btn_Html);
+            $(".product-form__buttons,.ProductForm").append($payment_btn_Html);
             clearInterval($findbuynowbtn);  
         }
         }
