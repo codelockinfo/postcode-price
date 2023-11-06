a5061ab885228540936db19b9e4ea3eda2410c62
codelockinfo <codelockinfo@gmail.com>
2023-11-06 16:59:33 +0530

    remove echo in get_postcode function

--- user/cls_functions.php
+++ user/cls_functions.php
@@ -536,10 +536,6 @@ class Client_functions extends common_function {
                 $combinedString = "";
                 if ($productdata && isset($productdata->product->variants)) {
                     foreach ($productdata->product->variants as $variant) {
-                        echo "<pre>";
-                        print_r($variant->price );
-                        echo "?????";
-                        print_r($zoneprice);
                     $totalprice = $variant->price + $zoneprice;
                     $combinedString .= $variant->id . "," . $totalprice .";";
 
