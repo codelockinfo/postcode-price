c9da6e2ff7e40a88c752ec9bccbb13ebc79e55f8
codelockinfo <codelockinfo@gmail.com>
2023-11-07 09:44:36 +0530

    variant1 put on variant array  & print  and echo add

--- user/cls_functions.php
+++ user/cls_functions.php
@@ -834,6 +834,7 @@ class Client_functions extends common_function {
                     }else if($variantitle == 3){
                         generate_log('createproduct', "  create product array option1&2"); 
                         $variants1 = array("option1"=>$varianttitle[0],"option2"=>$varianttitle[1],"option3"=>$varianttitle[2],"price" =>$productprice );
+                        array_push($variants,$variants1);
                         // foreach ($dynamicOption as $variableName => $variableValue) {
                         //     $options1 = array("name" => $variableValue,"position" => $variableName);
                         // }
