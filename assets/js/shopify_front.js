b4541985504fcd9ce391e38733878704e1e80631
codelockinfo <codelockinfo@gmail.com>
2023-11-29 15:51:32 +0530

    price display none css at the end of  getTotalprice function

--- assets/js/shopify_front.js
+++ assets/js/shopify_front.js
@@ -469,7 +469,7 @@ $(document).ready(function() {
                             $ProductPriceSymbol =  $.trim($productPriceClassHtml);
                             $currecySymbol = $ProductPriceSymbol.split(' ');
                             console.log($currecySymbol);
-                            $(".product__price-container .price__regular .price-item,.price__container,.price-wrapper,.product-page-price-wrp,.product-price,.t4s-product__price-review,.f8pr .f8pr-price,.product-single__prices,.product-single__meta .product__price,.ProductMeta__PriceList,.price-review,.product__price__wrap .product__price span,.price-list sale-price,.price-area,.price__regular .price-item--regular,.price .price__text,div span[data-product-price]").css("display","block");
+                            
                             setTimeout(function(){
                                 $(".product__price-container .price__regular .price-item,.price__container,.price-wrapper,.product-page-price-wrp,.product-price,.t4s-product__price-review .t4s-product-price,.f8pr .f8pr-price,.product-single__meta .product__price,.ProductMeta__PriceList,.price-review,.product__price__wrap .product__price span,.price-list sale-price,.product-page-info__price .price span,.price-area .current-price,.price__regular .price-item--regular,.price__number .money,div span[data-product-price]").html($currecySymbol[0]+" "+$newPrice);
                                 $("#postalholder").css({"opacity":"1","justify-content":"space-between"});
@@ -505,6 +505,7 @@ $(document).ready(function() {
                                         '<div class="clsremovezipcode" style="width: 30px;cursor: pointer;"><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/5000/svg"><path fill="currentColor" d="M352 192V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64H96a32 32 0 0 1 0-64h256zm64 0h192v-64H416v64zM192 960a32 32 0 0 1-32-32V256h704v672a32 32 0 0 1-32 32H192zm224-192a32 32 0 0 0 32-32V416a32 32 0 0 0-64 0v320a32 32 0 0 0 32 32zm192 0a32 32 0 0 0 32-32V416a32 32 0 0 0-64 0v320a32 32 0 0 0 32 32z"/></svg></div>'+
                                     '</div>');
                                 }
+                                $(".product__price-container .price__regular .price-item,.price__container,.price-wrapper,.product-page-price-wrp,.product-price,.t4s-product__price-review,.f8pr .f8pr-price,.product-single__prices,.product-single__meta .product__price,.ProductMeta__PriceList,.price-review,.product__price__wrap .product__price span,.price-list sale-price,.price-area,.price__regular .price-item--regular,.price .price__text,div span[data-product-price]").css("display","block");
                             },1500);
                         
                         }else{
