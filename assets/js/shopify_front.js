92f95ebd3988647efc753b36b6c8ee0364989826
codelockinfo <codelockinfo@gmail.com>
2023-11-29 12:56:56 +0530

    append placeholder class change

--- assets/js/shopify_front.js
+++ assets/js/shopify_front.js
@@ -75,7 +75,7 @@ include('https://code.jquery.com/jquery-3.6.0.min.js', function() {
                                                 popupHtml();
                                             }else{
                                                 console.log("onload event for pdppage");
-                                                $(".product__price-container,.price-wrapper,.product-page-price-wrp,.product-price,.t4s-product__price-review,.f8pr .f8pr-price,.product-single__prices,.product-single__meta .product__price,.ProductMeta__PriceList,.price-review,.product__price__wrap,.price--show-badge,.price-area,#product-price,.price .price__text,div[data-price-wrapper].my-3").append(
+                                                $(".product__price-container,.price-wrapper,.product-page-price-wrp,.product-price,.t4s-product__price-review,.f8pr .f8pr-price,.product-single__prices,.product-single__meta .product__price,.ProductMeta__PriceList,.price-review,.product__price__wrap,.price--show-badge,.price-area,.price__pricing-group,.price .price__text,div[data-price-wrapper].my-3").append(
                                                     '<div id="postalholder" style=" display:flex;width: 300px;border-bottom:2px solid #f5db00;padding:10px 15px 0px 15px;margin-bottom:10px;justify-content:space-between;>'+
                                                     ' <p class="pmessage">'+getCookie("postcodeval")+'<i class="icon-ok" style="color:#5b9b30;"></i>'+getCookie("postcodename")+'</p> '+  
                                                         '<div class="clsremovezipcode" style="width: 30px;cursor: pointer;"><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/5000/svg"><path fill="currentColor" d="M352 192V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64H96a32 32 0 0 1 0-64h256zm64 0h192v-64H416v64zM192 960a32 32 0 0 1-32-32V256h704v672a32 32 0 0 1-32 32H192zm224-192a32 32 0 0 0 32-32V416a32 32 0 0 0-64 0v320a32 32 0 0 0 32 32zm192 0a32 32 0 0 0 32-32V416a32 32 0 0 0-64 0v320a32 32 0 0 0 32 32z"/></svg></div>'+
@@ -459,7 +459,7 @@ include('https://code.jquery.com/jquery-3.6.0.min.js', function() {
                                         }
                                         $hasClasspostalholder = $("#postalholder").html();
                                         if($hasClasspostalholder == undefined){
-                                            $(".product__price-container,.price-wrapper,.product-page-price-wrp,.product-price,.t4s-product__price-review,.f8pr .f8pr-price,.product-single__prices,.product-page-info__price,.product-single__meta .product__price,.ProductMeta__PriceList,.price-review,.product__price__wrap,.price--show-badge,.price-area,#product-price,.price .price__text,div[data-price-wrapper].my-3").append(
+                                            $(".product__price-container,.price-wrapper,.product-page-price-wrp,.product-price,.t4s-product__price-review,.f8pr .f8pr-price,.product-single__prices,.product-page-info__price,.product-single__meta .product__price,.ProductMeta__PriceList,.price-review,.product__price__wrap,.price--show-badge,.price-area,.price__pricing-group,.price .price__text,div[data-price-wrapper].my-3").append(
                                                 '<div id="postalholder" style=" display:flex;width: 300px;border-bottom:2px solid #f5db00;padding:10px 15px 0px 15px;margin-bottom:10px;justify-content:space-between;>'+
                                                 ' <p class="pmessage">'+getCookie("postcodeval")+'<i class="icon-ok" style="color:#5b9b30;"></i>'+getCookie("postcodename")+'</p> '+  
                                                     '<div class="clsremovezipcode" style="width: 30px;cursor: pointer;"><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/5000/svg"><path fill="currentColor" d="M352 192V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64H96a32 32 0 0 1 0-64h256zm64 0h192v-64H416v64zM192 960a32 32 0 0 1-32-32V256h704v672a32 32 0 0 1-32 32H192zm224-192a32 32 0 0 0 32-32V416a32 32 0 0 0-64 0v320a32 32 0 0 0 32 32zm192 0a32 32 0 0 0 32-32V416a32 32 0 0 0-64 0v320a32 32 0 0 0 32 32z"/></svg></div>'+
@@ -575,10 +575,12 @@ include('https://code.jquery.com/jquery-3.6.0.min.js', function() {
             clearInterval($findbuynowbtn);  
         }
         }
+
         $.urlParam = function(name){
             var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
             return results[1] || 0;
         }
+        
         function getParameterByName(name, url) {
             if (!url) url = window.location.href;
             name = name.replace(/[[]]/g, "\\$&");
