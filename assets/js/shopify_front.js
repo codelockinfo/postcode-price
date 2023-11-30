43f775a3ffcf6ac995b9105fa42043b6c3dc5c58
codelockinfo <codelockinfo@gmail.com>
2023-11-30 10:29:51 +0530

    adobe theme  buy it now btn issue solve

--- assets/js/shopify_front.js
+++ assets/js/shopify_front.js
@@ -598,7 +598,7 @@ $(document).ready(function() {
             $(".shopify-payment-button__button").css("display","none");
             $(".shopify-payment-button__button").css("cssText", "display: none !important");
             $payment_btn_Html = '<button class="'+ $paytment_Btn_class +' clspayment">'+ $paytment_Btn_text +'</button>';
-            $(".product-form__buttons,.ProductForm,.product__submit__buttons,.payment-buttons,.dynamic-checkout,.buy-buttons .shopify-payment-button,.t4s-product-form__buttons .shopify-payment-button").append($payment_btn_Html);
+            $(".product-form__buttons:first,.ProductForm,.product__submit__buttons,.payment-buttons,.dynamic-checkout,.buy-buttons .shopify-payment-button,.t4s-product-form__buttons .shopify-payment-button").append($payment_btn_Html);
             $hasClassclspayment = $(".clspayment").html();
             if($hasClassclspayment == undefined){
                 $(".product__cart-functions .flex-buttons").append($payment_btn_Html);
